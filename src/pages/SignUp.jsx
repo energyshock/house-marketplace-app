219466fd7423eb2199659e19d0a5223c42030689
Manuel Brestak <manuel.brestak@gmail.com>
2022-05-03 14:26:07 +0200

    85. Register User

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {db} from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -24,7 +26,26 @@ function SignUp() {
       ...formData,
       [e.target.id]: e.target.value,
     })
+  }
+
+  const onSubmit = async (e) => {
     e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      });
+
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -37,7 +58,7 @@ function SignUp() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="text" className="nameInput" placeholder='Name' id='name' value={name} onChange={onChange} />
 
             <input type="email" className="emailInput" placeholder='Email' id='email' value={email} onChange={onChange} />
