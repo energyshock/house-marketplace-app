47266412aec1f9e9bfd61714f13d35fd6c29c0cd
Manuel Brestak <manuel.brestak@gmail.com>
2022-05-10 08:25:24 +0200

    93. Google OAuth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,6 +6,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from 'firebase/auth';
+import OAuth from '../components/OAuth';
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
@@ -121,7 +122,7 @@ function SignUp() {
             </div>
           </form>
 
-          {/* Google 0Auth */}
+          <OAuth />
 
           <Link to="/sign-in" className="registerLink">
             Sign In Instead
